fdc73c8425b287415f39820882afb3ebb06f0667
sneha <snehagaba233@gmail.com>
2026-08-21 12:41:40 +0530

    Added Regex Review code

---
+++ DotNetFellowship/Reviews/RegexReview.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Reviews
+{
+    internal class RegexReview
+    {
+        static void Main()
+        {
+            string input = "Sgsg$2526";
+            string regex = @"^([A-Z][a-z]{3})(\.|@|_|\^|\$)([0-9]{4})$";
+
+            bool var = Regex.IsMatch(input, regex);
+            Console.WriteLine(var);
+        }
+    }
+}
